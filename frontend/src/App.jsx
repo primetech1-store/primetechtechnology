bb55acf3062b004cf7351ae043cc7ae2f1beb628
Rai Kim <kim@host.client.com>
2025-05-12 20:18:56 +0300

    Initial commit: Add Stripe Payment API implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
 import "./App.css";
 import Payment from "./Payment";
 import Completion from "./Completion";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Items from "./components/Items";
 import Topbar from "./components/Topbar";
 import Login from "./Login";
 import Signup from "./Signup";
 import Carousel from "./components/carousel";
-import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <header>
         <img src="/logo.png" alt="Company Logo" />
         <nav>
@@ -21,18 +20,16 @@ function App() {
       </header>
 
       <main>
-        <BrowserRouter>
-          <Topbar />
-          <Routes>
-            <Route path="/" element={<><Carousel /><Items /></>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/completion" element={<Completion />} />
-          </Routes>
-        </BrowserRouter>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<><Carousel /><Items /></>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/completion" element={<Completion />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
