0859f89480222b615b10819149f8cdf147fc31b2
Rai Kim <kim@host.client.com>
2025-05-14 06:59:51 +0300

    Initial commit: Add Stripe Payment API implementation

--- frontend/src/components/Items.jsx
+++ frontend/src/components/Items.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import items from "/frontend/public/products.json";
+import items from "/frontend/products.json";
 import { Link } from "react-router-dom";
 
 const Items = () => {
